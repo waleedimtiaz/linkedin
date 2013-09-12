6b1ca1b90d2b537aaa0ce786d0cd9d5589f375ed
waleedimtiaz <waleedimtiaz@gmail.com>
2013-09-12 23:06:07 +0500

    trying to remove conflicts

---
+++ application/views/r/footer.php
@@ -0,0 +1,50 @@
+ <div class="footer">
+		   <div class="footer-content">
+		       <div class="footer-links">
+			     <a href="what_is_linkedin.html"><strong>Help Center</strong></a>
+				 <a href="what_is_linkedin.html">About</a>
+				 <a href="what_is_linkedin.html">Press</a>
+				 <a href="what_is_linkedin.html">Blogs</a>
+				 <a href="what_is_linkedin.html">Careers</a>
+				 <a href="what_is_linkedin.html">Advertising</a>
+				 <a href="what_is_linkedin.html">Talent Solutions</a>
+				 <a href="what_is_linkedin.html">Tools</a>
+				 <a href="what_is_linkedin.html">Mobile</a>
+				 <a href="what_is_linkedin.html">Developers</a>
+				 <a href="what_is_linkedin.html">Publishers</a>
+				 <a href="what_is_linkedin.html">Language</a>
+				 <a href="what_is_linkedin.html">SlideShare</a>
+			   
+			   </div>
+		   </div>
+		   
+		   <div class="footer-content">
+		       <div class="footer-links">
+			     <a href="what_is_linkedin.html">LinkedIn Updates</a>
+				 <a href="what_is_linkedin.html">LinkedIn Influencers</a>
+				 <a href="what_is_linkedin.html">LinkedIn Jobs</a>
+				 <a href="what_is_linkedin.html">Jobs Directory</a>
+				 <a href="what_is_linkedin.html">Company Directory</a>
+				 <a href="what_is_linkedin.html">Groups Directory</a>
+				 <a href="what_is_linkedin.html">Title Directory</a>
+				 
+			   </div>
+		   </div>
+		   <div class="footer-content">
+		        <div class="copyright">
+		            <div class="copyright-logo">
+			        </div>
+		        </div>
+				<div class="footer-links">
+			     <a href="what_is_linkedin.html">User Agreement</a>
+				 <a href="what_is_linkedin.html">Privacy Policy</a>
+				 <a href="what_is_linkedin.html">Community Guidelines</a>
+				 <a href="what_is_linkedin.html">Cookie Policy</a>
+				 <a href="what_is_linkedin.html">Copyright Policy</a>
+				 
+				 
+			   </div>
+				 
+		    </div>
+
+		</div>	
---
+++ application/views/r/signup.php
@@ -0,0 +1,83 @@
+<div class="container">
+		  <div class="main">
+			    <div class="static-left">
+				     <h1>Over 225 million professionals use LinkedIn to exchange information, ideas and opportunities</h1>
+				
+				    <div class="content-box-1">
+					  <div class="icon-reconnect"></div> <span class="icon-text">Stay informed about your contacts and industry</span>
+				    </div>
+				
+				    <div class="content-box-2">
+					  <div class="icon-answers"></div><span class="icon-text">Find the people & knowledge you need to achieve your goals</span>
+				    </div>
+				
+				    <div class="content-box-1">
+					  <div class="icon-power"></div><span class="icon-text">Control your professional identity online</span>
+				    </div>
+			    </div>
+			    <div class="form-right">
+			
+			    </div>
+			
+			    <div class="search-browser">
+				    <div class="search">
+					</div>
+					
+					<div class="directory">
+					    <h3>LinkedIn member directory:</h3>
+						<div class="directory-links">
+						    <a href="what_is_linkedin.html">a</a>
+							<a href="what_is_linkedin.html">b</a>
+							<a href="what_is_linkedin.html">c</a>
+							<a href="what_is_linkedin.html">d</a>
+							<a href="what_is_linkedin.html">e</a>
+							<a href="what_is_linkedin.html">f</a>
+							<a href="what_is_linkedin.html">g</a>
+							<a href="what_is_linkedin.html">h</a>
+							<a href="what_is_linkedin.html">i</a>
+							<a href="what_is_linkedin.html">j</a>
+							<a href="what_is_linkedin.html">k</a>
+							<a href="what_is_linkedin.html">l</a>
+							<a href="what_is_linkedin.html">m</a>
+							<a href="what_is_linkedin.html">n</a>
+							<a href="what_is_linkedin.html">o</a>
+							<a href="what_is_linkedin.html">p</a>
+							<a href="what_is_linkedin.html">q</a>
+							<a href="what_is_linkedin.html">r</a>
+							<a href="what_is_linkedin.html">s</a>
+							<a href="what_is_linkedin.html">t</a>
+							<a href="what_is_linkedin.html">u</a>
+							<a href="what_is_linkedin.html">v</a>
+							<a href="what_is_linkedin.html">w</a>
+							<a href="what_is_linkedin.html">x</a>
+							<a href="what_is_linkedin.html">y</a>
+							<a href="what_is_linkedin.html">z</a>
+							
+						</div>
+					
+					    <div class="country-search">
+					        <h3>Browse members</h3>
+						    <div class="link">
+							    <a href="what_is_linkedin.html">by country</a>
+						    </div>
+						    
+					    </div>
+					</div>
+				    <div class="agreement">
+					    <strong>*</strong>
+						By joining LinkedIn, you agree to linkedIn's
+						<div class="agreement-links">
+						   <a href="what_is_linkedin.html">User Agreement</a>
+						   ,
+						   <a href="what_is_linkedin.html">Privacy Policy</a>
+						   and
+						   <a href="what_is_linkedin.html">Cookie Policy</a>
+						   .
+						</div>
+					</div>
+					
+			
+			    </div>
+			
+		    </div>
+		</div>
